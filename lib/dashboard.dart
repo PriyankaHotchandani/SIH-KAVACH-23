b51d5010f3e66b98c561706400d07b5cfba5f319
dhairya903 <dhairya.shah@think360.ai>
2023-04-09 03:44:39 +0530

    Dashboard NFC read/write working from text input

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,13 +1,150 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
+
+import 'dart:convert' show utf8;
+
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
+
   @override
   _DashboardState createState() => _DashboardState();
 }
 
 /// Global flag if NFC is avalible
+bool isNfcAvalible = false;
 
 class _DashboardState extends State<Dashboard> {
+  bool listenerRunning = false;
+  bool writeCounterOnNextContact = false;
+  TextEditingController myController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () async {
+      isNfcAvalible = await NfcManager.instance.isAvailable();
+    });
+  }
+
+  Future<void> _listenForNFCEvents() async {
+    //Always run this for ios but only once for android
+    if (Platform.isAndroid && listenerRunning == false || Platform.isIOS) {
+      //Android supports reading nfc in the background, starting it one time is all we need
+      if (Platform.isAndroid) {
+        _alert(
+          'NFC listener running in background now, approach tag(s)',
+        );
+        //Update button states
+        setState(() {
+          listenerRunning = true;
+        });
+      }
+
+      NfcManager.instance.startSession(
+        onDiscovered: (NfcTag tag) async {
+          bool succses = false;
+          //Try to convert the raw tag data to NDEF
+          final ndefTag = Ndef.from(tag);
+          //If the data could be converted we will get an object
+          if (ndefTag != null) {
+            // If we want to write the current counter value we will replace the current content on the tag
+            if (writeCounterOnNextContact) {
+              //Ensure the write flag is off again
+              setState(() {
+                writeCounterOnNextContact = false;
+              });
+              //Create a 1Well known tag with en as language code and 0x02 encoding for UTF8
+              final ndefRecord = NdefRecord.createText(myController.text);
+              //Create a new ndef message with a single record
+              final ndefMessage = NdefMessage([ndefRecord]);
+              //Write it to the tag, tag must still be "connected" to the device
+              try {
+                //Any existing content will be overwritten
+                await ndefTag.write(ndefMessage);
+                _alert('Data written to the tag successfully');
+                myController.text = "";
+                succses = true;
+              } catch (e) {
+                _alert("Writing failed, press 'Write to tag' again");
+              }
+            }
+            //The NDEF Message was already parsed, if any
+            else if (ndefTag.cachedMessage != null) {
+              var ndefMessage = ndefTag.cachedMessage!;
+
+              //Each NDEF message can have multiple records, we will use the first one in our example
+              if (ndefMessage.records.isNotEmpty &&
+                  ndefMessage.records.first.typeNameFormat ==
+                      NdefTypeNameFormat.nfcWellknown) {
+                //If the first record exists as 1:Well-Known we consider this tag as having a value for us
+                final wellKnownRecord = ndefMessage.records.first;
+
+                ///Payload for a 1:Well Known text has the following format:
+                ///[Encoding flag 0x02 is UTF8][ISO language code like en][content]
+
+                if (wellKnownRecord.payload.first == 0x02) {
+                  //Now we know the encoding is UTF8 and we can skip the first byte
+                  final languageCodeAndContentBytes =
+                  wellKnownRecord.payload.skip(1).toList();
+                  //Note that the language code can be encoded in ASCI, if you need it be carfully with the endoding
+                  final languageCodeAndContentText =
+                  utf8.decode(languageCodeAndContentBytes);
+                  //Cutting of the language code
+                  final payload = languageCodeAndContentText.substring(2);
+                  //Parsing the content to int
+                  final storedCounters = payload.toString();
+                  succses = true;
+                  _alert(storedCounters);
+                }
+              }
+            }
+          }
+          //Due to the way ios handles nfc we need to stop after each tag
+          if (Platform.isIOS) {
+            NfcManager.instance.stopSession();
+          }
+          if (succses == false) {
+            _alert(
+              'Tag was not valid',
+            );
+          }
+        },
+        // Required for iOS to define what type of tags should be noticed
+        pollingOptions: {
+          NfcPollingOption.iso14443,
+          NfcPollingOption.iso15693,
+        },
+      );
+    }
+  }
+
+  void _alert(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+        ),
+        duration: const Duration(
+          seconds: 2,
+        ),
+      ),
+    );
+  }
+
+  void _writeNfcTag() {
+    setState(() {
+      writeCounterOnNextContact = true;
+    });
+
+    if (Platform.isAndroid) {
+      _alert('Approach phone with tag');
+    }
+    //Writing a requires to read the tag first, on android this call might do nothing as the listner is already running
+    _listenForNFCEvents();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,81 +159,102 @@ class _DashboardState extends State<Dashboard> {
         ),
         brightness: Brightness.light,
         elevation: 0,
-
       ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.all(18.0),
-              child: Text(
-                "Welcome to NFC Configurations \nSelect an option",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 5, 5, 5),
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.all(18.0),
+                child: Text(
+                  "Welcome to NFC Configurations \nSelect an option",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 5, 5, 5),
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.start,
                 ),
-                textAlign: TextAlign.start,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Center(
-                child: Wrap(
-                  spacing:20,
-                  runSpacing: 20.0,
-                  children: <Widget>[
-                    SizedBox(
-                      width:160.0,
-                      height: 160.0,
-                      child: Card(
-
-                        color: Color.fromARGB(255, 209, 208, 208),
-                        elevation: 2.0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0)
-                        ),
-                        child:Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(25.0),
-                            child: Column(
-                            children: <Widget>[
-                              Image.asset("assets/nfc-read-icon.png",width: 64.0,),
-                              const SizedBox(
-                                height: 8.0,
-                              ),
-                              const Text(
-                                "Start NFC Listener",
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 37, 35, 35),
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18.0
-                                ),
-                              ),
-                              
-                            ],
-                            ),
-                          )
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width:160.0,
-                      height: 160.0,
-                      child: Card(
-
-                        color: Color.fromARGB(255, 209, 208, 208),
-                        elevation: 2.0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0)
-                        ),
-                        child:Center(
-                            child: Padding(
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(labelText: 'Enter data in NFC'),
+                  controller: myController,
+                  onEditingComplete: () {
+                    _writeNfcTag();
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Center(
+                  child: Wrap(
+                    spacing: 20,
+                    runSpacing: 20.0,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 160.0,
+                        height: 160.0,
+                        child: Card(
+                          color: Color.fromARGB(255, 209, 208, 208),
+                          elevation: 2.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                          child: InkWell(
+                            onTap: _listenForNFCEvents,
+                            // onTap: () {
+                            //   print("tapped start nfc listener");
+                            // },
+                            child: Center(
+                                child: Padding(
                               padding: const EdgeInsets.all(25.0),
                               child: Column(
                                 children: <Widget>[
-                                  Image.asset("assets/write-tag.png",width: 64.0,),
+                                  Image.asset(
+                                    "assets/nfc-read-icon.png",
+                                    width: 60.0,
+                                  ),
+                                  const SizedBox(
+                                    height: 8.0,
+                                  ),
+                                  const Text(
+                                    "Start NFC Listener",
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 37, 35, 35),
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18.0),
+                                  ),
+                                ],
+                              ),
+                            )),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 160.0,
+                        height: 160.0,
+                        child: Card(
+                          color: Color.fromARGB(255, 209, 208, 208),
+                          elevation: 2.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                          child: InkWell(
+                            onTap: _writeNfcTag,
+                            child: Center(
+                                child: Padding(
+                              padding: const EdgeInsets.all(25.0),
+                              child: Column(
+                                children: <Widget>[
+                                  Image.asset(
+                                    "assets/write-tag.png",
+                                    width: 46.0,
+                                  ),
                                   const SizedBox(
                                     height: 8.0,
                                   ),
@@ -105,31 +263,35 @@ class _DashboardState extends State<Dashboard> {
                                     style: TextStyle(
                                         color: Color.fromARGB(255, 37, 35, 35),
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 18.0
-                                    ),
+                                        fontSize: 18.0),
                                   ),
                                 ],
                               ),
-                            )
+                            )),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width:160.0,
-                      height: 160.0,
-                      child: Card(
-
-                        color: Color.fromARGB(255, 209, 208, 208),
-                        elevation: 2.0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0)
-                        ),
-                        child:Center(
-                            child: Padding(
+                      SizedBox(
+                        width: 160.0,
+                        height: 160.0,
+                        child: Card(
+                          color: Color.fromARGB(255, 209, 208, 208),
+                          elevation: 2.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                          child: InkWell(
+                            onTap: () {
+                              _alert("Display data from the tag");
+                            },
+                            child: Center(
+                                child: Padding(
                               padding: const EdgeInsets.all(25.0),
                               child: Column(
                                 children: <Widget>[
-                                  Image.asset("assets/edit-metadata.png",width: 64.0,),
+                                  Image.asset(
+                                    "assets/edit-metadata.png",
+                                    width: 64.0,
+                                  ),
                                   const SizedBox(
                                     height: 8.0,
                                   ),
@@ -138,31 +300,35 @@ class _DashboardState extends State<Dashboard> {
                                     style: TextStyle(
                                         color: Color.fromARGB(255, 37, 35, 35),
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 18.0
-                                    ),
+                                        fontSize: 18.0),
                                   ),
                                 ],
                               ),
-                            )
+                            )),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width:160.0,
-                      height: 160.0,
-                      child: Card(
-
-                        color: Color.fromARGB(255, 209, 208, 208),
-                        elevation: 2.0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0)
-                        ),
-                        child:Center(
-                            child: Padding(
+                      SizedBox(
+                        width: 160.0,
+                        height: 160.0,
+                        child: Card(
+                          color: Color.fromARGB(255, 209, 208, 208),
+                          elevation: 2.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                          child: InkWell(
+                            onTap: () {
+                              print("Tapped Info");
+                            },
+                            child: Center(
+                                child: Padding(
                               padding: const EdgeInsets.all(25.0),
                               child: Column(
                                 children: <Widget>[
-                                  Image.asset("assets/settings.png",width: 64.0,),
+                                  Image.asset(
+                                    "assets/settings.png",
+                                    width: 64.0,
+                                  ),
                                   const SizedBox(
                                     height: 8.0,
                                   ),
@@ -171,22 +337,31 @@ class _DashboardState extends State<Dashboard> {
                                     style: TextStyle(
                                         color: Color.fromARGB(255, 37, 35, 35),
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 18.0
-                                    ),
+                                        fontSize: 18.0),
                                   ),
                                 ],
                               ),
-                            )
+                            )),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
-        )
-      )
+              )
+            ],
+          ),
+        ),
+      ),
     );
+  }
+  @override
+  void dispose() {
+    try {
+      NfcManager.instance.stopSession();
+    } catch (_) {
+      //We dont care
+    }
+    super.dispose();
   }
 }
