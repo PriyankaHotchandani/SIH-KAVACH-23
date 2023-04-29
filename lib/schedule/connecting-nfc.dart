190455dc80971dcd440eb86229ded5a27b1f749c
KrutikaBhatt <krutikabhatt222@gmail.com>
2023-04-29 15:52:14 +0530

    added the Connecting NFC page

--- lib/schedule/connecting-nfc.dart
+++ lib/schedule/connecting-nfc.dart
@@ -33,13 +33,48 @@ class ConnectBulkNFC extends StatelessWidget {
                   scale: 2.5,),
 
               const SizedBox(
-                height: 50,
+                height: 10,
               ),
               const Center(
                 child: Text(
                   'Approach the NFC Tag',
                   style: TextStyle(color: Colors.black,fontSize: 25, fontWeight: FontWeight.w300),
                 ),
+              ),
+              Container(
+                height: 140,
+                width: double.infinity,
+                //color: const Color.fromARGB(255, 194, 192, 192),
+                margin: const EdgeInsets.all(30),
+                padding: const EdgeInsets.all(10),
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 231, 228, 228),
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                child: const Text("Officer ID: A1244\n\nName: Prashant Mohite\n\nStation:Navpada Police Station\n\nContact: 93453843445",
+                    style: TextStyle(fontSize: 14)),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 60.0, right: 60.0),
+                child: Row(
+                children: <Widget>[
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                    primary: Colors.red),
+                    child: const Text(
+                      "Write NFC Tag",
+                    ),
+                    ),
+                    const SizedBox(
+                      width: 30
+                    ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Read Tag"),
+                  ),
+                ],
+              ),
               ),
               
             ], //<Widget>[]
